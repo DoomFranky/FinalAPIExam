3f0ed6230745957eb438782fb0401f125068066a
Randy <raoulrandy3@gmail.com>
2025-09-03 10:02:47 +0300

    feat: finish some thing

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+from type import List
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
 
-class Phones:
-    id: String
-    brand: String
-    models: String
+class phones(BaseModel):
+    id: str
+    brand: str
+    models: str
     class characteristique:
         ram_memory: int
         rom_memory: int
 
+phone= List(Phones)
 
 app = FastAPI()
 
@@ -17,16 +22,23 @@ def ping():
     return Response(content='OK',status_code=200,media_type='text/plain')
 
 @app.post("/phones")
-def postPhones():
-    return null
+def postPhones(thePhone = List(Phones)):
+    thePhone.extend()
+    return thePhone
 
+def serializing():
+    serialized = []
+    for p in phone:
+        serialized.append(p.model_dump())
+    return serialized
 @app.get("/phones")
 def getPhones():
-    return null
+    return JSONResponse(content=serializing(),status_code='200',media_type='application/json')
 
 @app.get("/phones")
 def getPhonesById(id: Request):
-    return null
+    thePhone: serializing().id
+    return JSONResponse(content=thePhone,status_code='200',media_type='application/json')
 @app.put("/phones")
 def getPhonesById(id :Request):
     return null
